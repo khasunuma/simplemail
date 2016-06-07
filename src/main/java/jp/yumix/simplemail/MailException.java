6e7224fba98bf4142b8df35f40a394ffcebaa5e3
Yumi Hiraoka <yumix@outlook.com>
2016-06-07 09:13:19 +0900

    Setup project

---
+++ src/main/java/jp/yumix/simplemail/MailException.java
@@ -0,0 +1,34 @@
+package jp.yumix.simplemail;
+
+import javax.mail.MessagingException;
+
+/**
+ * Exception thrown when some error occurred.
+ * 
+ * @author Yumi Hiraoka
+ *
+ */
+public class MailException extends RuntimeException {
+
+    /**
+     * Value of serialVersionUID.
+     */
+    private static final long serialVersionUID = -397978964640697847L;
+
+    /**
+     * Default constructor.
+     */
+    public MailException() {
+        super();
+    }
+
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause cause
+     */
+    public MailException(MessagingException cause) {
+        super(cause.getMessage(), cause);
+    }
+
+}
---
+++ src/test/java/jp/yumix/simplemail/MailExceptionTest.java
@@ -0,0 +1,19 @@
+package jp.yumix.simplemail;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MailExceptionTest {
+
+    @Test
+    public void testMailException() {
+        fail("まだ実装されていません");
+    }
+
+    @Test
+    public void testMailExceptionMessagingException() {
+        fail("まだ実装されていません");
+    }
+
+}
